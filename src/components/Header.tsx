e96bce920f45b469d2de21c90cf9df1b8c7eeeac
BrMD <dorigon.matheus8@gmail.com>
2023-08-28 14:32:44 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div className="flex justify-between">
+      <div className="m-6">logo</div>
+      <div className="flex m-6">
+        <div className="mx-2">Inicio</div>
+        <div className="mx-2">Stack</div>
+        <div className="mx-2">Projetos</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
